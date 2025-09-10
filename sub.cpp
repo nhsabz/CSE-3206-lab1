be807579c0c92e0769037c07d579a4267f6dc44c
nhsabz <hudasabbir@gmail.com>
2025-09-10 11:56:53 +0600

    Updated sub.cpp with new logic

---
+++ sub.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    int result = a - b;
+    cout << "Subtraction: " << result << endl;
+    return 0;
+}
